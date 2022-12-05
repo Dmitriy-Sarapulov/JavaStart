d7a1183eae03a7d92ee4438b485e831962fd04ae
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2022-12-05 21:14:07 +0300

    final1 commit

--- program.java
+++ program.java
@@ -54,3 +54,18 @@
 //     }
 
 // }
+// чтение из файла
+// import java.io.*;
+// public class Program {
+// public static void main(String[] args) throws Exception {
+// FileReader fr = new FileReader("file.txt");
+// int c;
+// while ((c = fr.read()) != -1) {
+// char ch = (char) c;
+// if (ch == '\n') {
+// System.out.print(ch);
+// } else {
+// System.out.print(ch);
+// }
+// }
+// } }
