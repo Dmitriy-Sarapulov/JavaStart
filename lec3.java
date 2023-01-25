f8da958d279655c25346f15dbcc98578b3ae448e
Dmitriy Sarapulov <sarapulovdima@mail.ru>
2023-01-25 19:40:32 +0300

    final commit2

--- lec3.java
+++ lec3.java
@@ -1,3 +1,16 @@
+// Java: знакомство и как пользоваться базовым API (лекции)
+// Тип данных Object - всему голова
+// Упаковка - любой тип можно положить  в переменную типа Object
+// Распаковка - преобразование Object-переменной в нужный тип
+// Иерархия типов - любой тип "ниже" Object'a
+// Массивы java System.arraycopy(копирование массива) (a, 0, b, 0, a.length);
 public class lec3 {
-    
+    public static void main(String[] args) {
+            Object o = 1; GetType(o); // java.lang.Integer
+            o = 1.2; GetType(o); // java.lang.Double
+               
+    }
+    static void GetType(Object obj){
+        System.out.println(obj.getClass().getName());
+    }
 }
